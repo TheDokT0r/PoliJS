5f63b56721b3782546b3230085587da3e374ab5c
Orche <orkv1234@gmail.com>
2023-08-12 15:14:44 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+
+console.log(chalk.bgGreen('Hello World!') + chalk.bgBlue('Hello World!'));
+chalkAnimation.rainbow('Hello World!');
